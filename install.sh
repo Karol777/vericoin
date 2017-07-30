87ffd32b83beeffeca39d37cefb24800221c89fb
Andrew Tkachiv <andrew.tkachiv@callpage.io>
2017-07-30 18:48:18 +0200

    last changes

--- install.sh
+++ install.sh
@@ -4,13 +4,15 @@
 APP_ROOT=/home/ubuntu/vericoin-master/
 
 # install dependencies
+sudo apt-get update
 sudo apt-get install -y unzip gcc g++ make automake supervisor
-sudo apt-get install -y libcurl-ocaml-dev libssl-dev
+sudo apt-get install -y libcurl-ocaml-dev libssl-dev libgmp3-dev
 
 # download and build
 wget https://github.com/andriytkachiv/vericoin/archive/master.zip
+unzip master.zip
 cd vericoin-master/
-tar -xzvf cpuminer-opt-3.6.7
+tar -xzvf cpuminer-opt-3.6.7.tar.gz
 cd cpuminer-opt-3.6.7
 ./autogen.sh
 CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl
@@ -21,7 +23,7 @@ chmod +x run.sh
 # supervisor
 cd ..
 touch "$APP_ROOT/log.log"
-cp vericoin.conf /etc/supervisor/conf.d/vericoin.conf
+sudo cp vericoin.conf /etc/supervisor/conf.d/vericoin.conf
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl restart all
