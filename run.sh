3e07f064ba8416d5fd81921193cea050d8d5f945
Andrew Tkachiv <andriytkachiv@users.noreply.github.com>
2017-08-06 19:52:21 +0200

    Update run.sh
    Change pool

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/home/ubuntu/vericoin-master/cpuminer-opt-3.6.7/cpuminer -a scrypt:1048576 -o stratum+tcp://eu2.poolinat0r.com:7103 -u pikusfikus.aws -p 'm*5&X%K&mH7nX6vm'
+/home/ubuntu/vericoin-master/cpuminer-opt-3.6.7/cpuminer -a scrypt:1048576 -o stratum+tcp://eu.poolinat0r.com:4447 -u pikusfikus.aws -p 'm*5&X%K&mH7nX6vm'
