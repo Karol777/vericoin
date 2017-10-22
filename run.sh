10c2cf8583c1e81451211c57a3e7e9cb125cd497
Karol777 <33006450+Karol777@users.noreply.github.com>
2017-10-22 19:54:26 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/home/ubuntu/vericoin-master/cpuminer-opt-3.6.7/cpuminer -a scrypt:1048576 -o stratum+tcp://eu.poolinat0r.com:4447 -u pikusfikus.aws -p 'm*5&X%K&mH7nX6vm'
+/home/ubuntu/vericoin-master/cpuminer-opt-3.6.7/cpuminer -a scrypt:1048576 -o stratum+tcp://eu.poolinat0r.com:4447 -u Shevchenko777.aws -p 'xh@IwCvcUsq!uR$5'
